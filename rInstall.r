ddef2fd4264b92bdba528245d925f4ac2afbab40
Yuan-Hsin Huang <yuanhsin7211@gmail.com>
2016-09-15 15:26:16 -0700

    Create rInstall.r

---
+++ rInstall.r
@@ -0,0 +1,36 @@
+# understanding the business problem
+  # identify potential areas where improvement is required
+  # look for available data to support the initiative
+  # if data not available, look for alternative data
+  # if alternative data not found, look for open data (govt. data)
+
+
+# once you identify data
+# convert a business problem to a statistical/mathematical problem
+  # by use of hypothesis
+  # how to frame a hypothesis
+  # how to validate a hypothesis
+  # how to conclude a hypothesis
+
+# apply predictive models to get a statistical solution
+  # data cleaning
+  # feature engineering
+  # feature creation
+  # feature reduction
+  # data partition
+  # model training 
+  # model validation on the dataset
+
+# converting / transforming statistical solution to a real life solution
+  # interpreting the model results
+  # forming business strategies
+  # identifying potential areas to implement the recommendations
+
+
+## Install R and R studio(Recommander,Rattle)
+# R commander
+library(Rcmdr)
+
+# R data miner
+library(rattle)
+rattle()
